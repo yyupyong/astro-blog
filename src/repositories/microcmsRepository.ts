b1532bd9abecae5fb9f705790fda63aaa81bcd94
kim <yupyong.kim@gmail.com>
2023-12-31 12:00:00 +0900

    fix:repository

--- src/repositories/microcmsRepository.ts
+++ src/repositories/microcmsRepository.ts
@@ -1,18 +1,37 @@
 import type { MicroCMSQueries } from "microcms-js-sdk";
-import type { Blog, BlogResponse } from "../types/microcmsTypes";
+import type { BlogsData, BlogsDetail } from "../types/microcmsTypes";
 import { client } from "../library/microcms";
 
-//APIの呼び出し
-export const getBlogs = async (queries?: MicroCMSQueries) => {
-  return await client.get<BlogResponse>({ endpoint: "blogs", queries });
+// Get list API data
+export const getAllBlogs = async (
+  queries: MicroCMSQueries
+): Promise<BlogsData> => {
+  return await client
+    .get({
+      endpoint: "blogs",
+      queries,
+    })
+    .then((res) => {
+      console.log(res, "<<< response all data");
+      return res;
+    })
+    .catch((err) => console.error(err));
 };
-export const getBlogDetail = async (
+
+// Get list API detail data
+export const getBlogsDetail = async (
   contentId: string,
   queries?: MicroCMSQueries
-) => {
-  return await client.getListDetail<Blog>({
-    endpoint: "blogs",
-    contentId,
-    queries,
-  });
+): Promise<BlogsDetail> => {
+  return await client
+    .getListDetail({
+      endpoint: import.meta.env.ENDPOINT,
+      contentId,
+      queries,
+    })
+    .then((res) => {
+      console.log(res, "<<< response detail data");
+      return res;
+    })
+    .catch((err) => console.error(err));
 };
