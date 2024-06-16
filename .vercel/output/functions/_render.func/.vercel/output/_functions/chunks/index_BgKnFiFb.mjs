a8181892e9167e10e4b913fb50b10e1d3ace18ee
kim <yupyong.kim@gmail.com>
2024-06-16 21:49:41 +0900

    build

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_BgKnFiFb.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_BgKnFiFb.mjs
@@ -1,5 +1,5 @@
 export { renderers } from '../renderers.mjs';
 
-const page = () => import('./pages/index_DOcZWUUw.mjs');
+const page = () => import('./pages/index_Dop5kagm.mjs');
 
 export { page };
