ef90ba65f35661462e71216072086c01c36c6aa2
kim <yupyong.kim@gmail.com>
2024-06-16 21:05:11 +0900

    fix: response model class

--- src/types/microcmsTypes.ts
+++ src/types/microcmsTypes.ts
@@ -9,21 +9,35 @@ export type BlogsDetail = {
 };
 
 export type BlogsData = {
-  contents: [
-    {
-      id: string;
-      title: string;
-      createdAt: string;
-      url: BlogUrl;
-    }
-  ];
+  contents: BlogContent[];
   totalCount: number;
   offset: number;
   limit: number;
+};
+
+export type BlogContent = {
+  id: string;
+  title: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  content: string;
+  eyecatch?: BlogUrl; // eyecatchは任意のフィールドとする
+  category?: BlogCategory; // categoryは任意のフィールドとする
 };
 
 export type BlogUrl = {
   url: string;
   height: number;
   width: number;
+};
+
+export type BlogCategory = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  name: string;
 };
